3d316a8654610aec31108f5febb39bd98d111f9c
Vek <54672226+vivekstyles@users.noreply.github.com>
2021-02-12 09:58:09 +0530

    Delete round_1.cpp

--- round_1.cpp
+++
@@ -1,8 +0,0 @@
-#include "iostream"
-using namespace std;
-int main(){
-    char input[1024];
-    cout <<"enter";
-    cin >> input; // Get user input from the keyboard
-    cout << "Your number is: " << input; // Display the input value
-}
