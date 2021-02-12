2ff144f642a35dd6e0fabb31545569e775e78a55
Vek <54672226+vivekstyles@users.noreply.github.com>
2021-02-12 09:55:27 +0530

    Add files via upload

---
+++ round_1.cpp
@@ -0,0 +1,8 @@
+#include "iostream"
+using namespace std;
+int main(){
+    char input[1024];
+    cout <<"enter";
+    cin >> input; // Get user input from the keyboard
+    cout << "Your number is: " << input; // Display the input value
+}
